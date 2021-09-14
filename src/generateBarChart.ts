b2d042bc1358413bba602e7718795d18e8b353ca
Hyunwoo Seo <sraphic@kakao.com>
2021-09-15 03:27:19 +0900

    Update generateBarChart.ts

--- src/generateBarChart.ts
+++ src/generateBarChart.ts
@@ -4,12 +4,12 @@
 export default function generateBarChart(percent: number, size: number) {
   const syms = '░▏▎▍▌▋▊▉█';
 
-  const frac = Math.floor((size * 7 * percent) / 100);
-  const barsFull = Math.floor(frac / 7);
+  const frac = Math.floor((size * 9 * percent) / 100);
+  const barsFull = Math.floor(frac / 9);
   if (barsFull >= size) {
     return syms.substring(8, 9).repeat(size);
   }
-  const semi = frac % 7;
+  const semi = frac % 9;
 
   return [syms.substring(8, 9).repeat(barsFull), syms.substring(semi, semi + 1)]
     .join('')
